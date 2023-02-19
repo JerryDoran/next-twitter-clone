47f78697186e28a8e63d96e14b41f591f7b00281
Jerome Doran <disneyfan9184@gmail.com>
2023-02-19 11:55:13 -0500

    added react-modal for reply comments on a post

--- atom/modalAtom.js
+++ atom/modalAtom.js
@@ -4,3 +4,15 @@ export const modalState = atom({
   key: 'modalState',
   default: false,
 });
+
+export const postIdState = atom({
+  key: 'postIdState',
+  default: 'id',
+});
+
+
+// The below error occurs because the document ID has an empty string for the initial state.
+// To fix this error we need to set the inital state to a value
+//-----------------------------------------------------------------------------------------
+// Unhandled Runtime Error
+// FirebaseError: Invalid document reference. Document references must have an even number of segments, but posts has 1.
--- components/feed/Post.js
+++ components/feed/Post.js
@@ -21,13 +21,14 @@ import { signIn, useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { deleteObject, ref } from 'firebase/storage';
 import { useRecoilState } from 'recoil';
-import { modalState } from '../../atom/modalAtom';
+import { modalState, postIdState } from '../../atom/modalAtom';
 
 export default function Post({ post }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
 
   useEffect(() => {
     const unsub = onSnapshot(
@@ -110,7 +111,14 @@ export default function Post({ post }) {
         <div className='flex justify-between text-gray-500 p-2'>
           <div>
             <ChatIcon
-              onClick={() => setOpen(!open)}
+              onClick={() => {
+                if (!session) {
+                  signIn();
+                } else {
+                  setPostId(post.id);
+                  setOpen(!open);
+                }
+              }}
               className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'
             />
           </div>
